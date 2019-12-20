3e13dac8a20c528507206fdf21911c314826f12f
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2019-12-20 21:19:54 +0200

    added open and close connection functions

---
+++ db-connection.php
@@ -0,0 +1,24 @@
+<?php
+
+function openConnection()
+{
+    $localhost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "projectmanager";
+    $dbPort = 3306;
+
+    $conn = new mysqli($localhost, $dbUsername, $dbPassword, $dbName, $dbPort);
+
+    if ($conn->connect_error)
+        die("Connection failed: ".$conn->connect_error);
+
+    return $conn;
+}
+
+function closeConnection($conn)
+{
+    $conn->close();
+}
+
+?>
--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 <body>
 <div id="header">
     <h1>Welcome to Project Manager</h1>
+    <br>
+</div>
+<div id="addNewProject">
+    <a href="/project-manager/addProject.php">Add New Project</a>
+    <br>
 </div>
 </body>
 
